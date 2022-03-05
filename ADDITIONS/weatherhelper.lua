9b4a30e6d53e4647dd1298e989ee455c8ccd8c23
mkhamster <68155220+mkhamster@users.noreply.github.com>
2022-03-05 16:30:28 +0100

    Create weatherhelper.lua

---
+++ ADDITIONS/weatherhelper.lua
@@ -0,0 +1,8 @@
+game:GetService("RunService").RenderStepped:Connect(function()
+	game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Grass,game.Workspace["AX+2"].TerrainValues.GrassColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Asphalt,game.Workspace["AX+2"].TerrainValues.AsphaltColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Sand,game.Workspace["AX+2"].TerrainValues.SandColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Mud,game.Workspace["AX+2"].TerrainValues.MudColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Ground,game.Workspace["AX+2"].TerrainValues.GroundColor.Value)
+end)
