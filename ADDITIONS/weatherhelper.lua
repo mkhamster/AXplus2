c469fa1aff8ccba9cc2f906516ef69e6069b4cef
mkhamster <68155220+mkhamster@users.noreply.github.com>
2022-03-07 18:22:28 +0100

    Update weatherhelper.lua

--- ADDITIONS/weatherhelper.lua
+++ ADDITIONS/weatherhelper.lua
@@ -1,8 +1,8 @@
 game:GetService("RunService").RenderStepped:Connect(function()
-	game.Workspace["AX+2"][".Misc"].rain.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Grass,game.Workspace["AX+2"][".Misc"].TerrainValues.GrassColor.Value)
-	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Asphalt,game.Workspace["AX+2"][".Misc"].TerrainValues.AsphaltColor.Value)
-	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Sand,game.Workspace["AX+2"][".Misc"].TerrainValues.SandColor.Value)
-	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Mud,game.Workspace["AX+2"][".Misc"].TerrainValues.MudColor.Value)
-	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Ground,game.Workspace["AX+2"][".Misc"].TerrainValues.GroundColor.Value)
+	game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Grass,game.Workspace["AX+2"].TerrainValues.GrassColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Asphalt,game.Workspace["AX+2"].TerrainValues.AsphaltColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Sand,game.Workspace["AX+2"].TerrainValues.SandColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Mud,game.Workspace["AX+2"].TerrainValues.MudColor.Value)
+	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Ground,game.Workspace["AX+2"].TerrainValues.GroundColor.Value)
 end)
