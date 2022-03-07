79dbfa9f7cf0989347cf40c6246e874a49e1c7b7
mkhamster <68155220+mkhamster@users.noreply.github.com>
2022-03-07 20:40:32 +0100

    Update weatherhelper.lua

--- ADDITIONS/weatherhelper.lua
+++ ADDITIONS/weatherhelper.lua
@@ -1,4 +1,4 @@
-wait(5)
+--[[wait(5)
 game:GetService("RunService").RenderStepped:Connect(function()
 	game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.LowerTorso.Position
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Grass,game.Workspace["AX+2"].TerrainValues.GrassColor.Value)
@@ -7,3 +7,10 @@ game:GetService("RunService").RenderStepped:Connect(function()
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Mud,game.Workspace["AX+2"].TerrainValues.MudColor.Value)
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Ground,game.Workspace["AX+2"].TerrainValues.GroundColor.Value)
 end)
+]]
+wait(5)
+while wait() do
+    if game.Workspace:FindFirstChild("AX+2"):FindFirstChild("rain") then
+        game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.LowerTorso.Position
+    end
+end
