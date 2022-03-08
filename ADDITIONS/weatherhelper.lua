c58d0a191ed8348dec7ac7a2581f30ffe23d6001
mkhamster <68155220+mkhamster@users.noreply.github.com>
2022-03-08 17:05:34 +0100

    Update weatherhelper.lua

--- ADDITIONS/weatherhelper.lua
+++ ADDITIONS/weatherhelper.lua
@@ -1,16 +1,18 @@
---[[wait(5)
+wait(5)
 game:GetService("RunService").RenderStepped:Connect(function()
-	game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.LowerTorso.Position
+	if game.Workspace["AX+2"]:FindFirstChild("rain") then
+		game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.LowerTorso.Position	
+	end
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Grass,game.Workspace["AX+2"].TerrainValues.GrassColor.Value)
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Asphalt,game.Workspace["AX+2"].TerrainValues.AsphaltColor.Value)
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Sand,game.Workspace["AX+2"].TerrainValues.SandColor.Value)
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Mud,game.Workspace["AX+2"].TerrainValues.MudColor.Value)
 	game.Workspace.Terrain:SetMaterialColor(Enum.Material.Ground,game.Workspace["AX+2"].TerrainValues.GroundColor.Value)
 end)
-]]
-wait(5)
+
+--[[wait(5)
 while wait() do
     if game.Workspace:FindFirstChild("AX+2"):FindFirstChild("rain") then
         game.Workspace["AX+2"].rain.Position = game.Players.LocalPlayer.Character.LowerTorso.Position
     end
-end
+end]]
