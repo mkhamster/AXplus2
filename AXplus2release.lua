007d14b046beaad7d4f000af69a04bb2651a40d0
mkhamster <68155220+mkhamster@users.noreply.github.com>
2022-03-09 16:07:10 +0100

    Update AXplus2release.lua

--- AXplus2release.lua
+++ AXplus2release.lua
@@ -1,15 +1,14 @@
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map1.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map2.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map3.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map4.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map5.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map6.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map7.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map8.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map9.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map10.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map11.lua'))() end)
-pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/CHUNKDATA/map12.lua'))() end)
+local chunks = 31
+local c = 0
+
+repeat
+      c = c +1
+      pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/MAP/Script'..tostring(c)..'.lua'))() end)
+      wait()
+until c == chunks
+
+
+
 
 if _G.newLighting == true then
    local shader = loadstring(game:HttpGet('https://raw.githubusercontent.com/mkhamster/AXplus2/main/ADDITIONS/shader.lua'))()
